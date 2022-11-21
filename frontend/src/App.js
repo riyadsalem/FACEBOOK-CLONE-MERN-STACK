36dfcbddec366ecf7968980a22e2fd55eab3c22a
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-11-21 09:53:57 +0200

    SETUP THE BASIC FRONTEND

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import { Home } from "./svg";
+const App = () => {
+  return (
+    <>
+      <div>
+        WELCOME FRONTEND <br />
+        <Home />
+        <i className="friends_suggestions_icon"></i>
+      </div>
+    </>
+  );
+};
+
+export default App;
