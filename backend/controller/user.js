010faa48cd0bc10734bfa0fba0a303afafba580a
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-12-11 16:51:11 +0200

    Login Release #3

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -93,3 +93,39 @@ exports.activateAccount = async (req, res) => {
     });
   }
 };
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({
+        message:
+          "The email address you entered is not connected to an account.",
+      });
+    }
+
+    const check = await bcrypt.compare(password, user.password);
+    if (!check) {
+      res
+        .status(400)
+        .json({ message: "Invalid credentials. Please try again." });
+    }
+
+    const token = generateToken({ id: user._id.toString() }, "7d");
+
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: token,
+      verified: user.verified,
+      message: "Register Success ! please activate your email to start",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
