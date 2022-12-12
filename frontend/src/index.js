e5305da9f81436a8bf4a2bd7a9f6fd74cb4051d0
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-12-12 21:46:59 +0200

    React Redux store explained and full setup Release #5

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,17 +4,14 @@ import "./index.css";
 import "./styles/icons/icons.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import Reducers from "../src/store/reducer";
+// import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
 
-// Store
-/*
-const store = createStore(counter);
-store.subscribe(() => console.log(store.getState()));
-*/
-const store = createStore(Reducers, composeWithDevTools);
+const store = configureStore({
+  reducer: { rootReducer },
+});
 
 ReactDOM.render(
   <Provider store={store}>
--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,24 +1,9 @@
-import { useSelector, useDispatch } from "react-redux";
-import { Increment, Decrement, Login, Logout } from "../../store/actions";
-
-const App = () => {
-  const count = useSelector((state) => state.counter.count);
-  const isLogged = useSelector((state) => state.logged.logged);
-  const dispatch = useDispatch();
+export default function Login() {
   return (
     <>
       <div>
-        {isLogged ? (
-          <button onClick={() => dispatch(Logout())}>Logout</button>
-        ) : (
-          <button onClick={() => dispatch(Login())}>Login</button>
-        )}
-        <h1>{count}</h1>
-        <button onClick={() => dispatch(Increment())}>+</button>
-        <button onClick={() => dispatch(Decrement())}>-</button>
+        <h1>This is Login Page</h1>
       </div>
     </>
   );
-};
-
-export default App;
+}
