8ec8bf3f93fefefb73d86dde12949c085ed52932
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-12-12 19:00:39 +0200

    TEST CODE FOR REDUX LIFECYCLE >>> USED REDUX BACKAGE

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,44 @@ import "./index.css";
 import "./styles/icons/icons.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+// Action
+const increment = () => {
+  return {
+    type: "INC",
+    payload: 2,
+  };
+};
+
+const Decrement = () => {
+  return {
+    type: "DEC",
+    payload: 1,
+  };
+};
+
+// Reducer
+const counter = (statue = 0, action) => {
+  switch (action.type) {
+    case "INC":
+      return statue + action.payload;
+    case "DEC":
+      return statue + action.payload;
+    default:
+      return statue;
+  }
+};
+
+// Store
+const store = createStore(counter);
+store.subscribe(() => console.log(store.getState()));
+
+// Desbach
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(Decrement());
 
 ReactDOM.render(
   <Router>
