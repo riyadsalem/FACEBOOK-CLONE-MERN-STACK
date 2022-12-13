b3faa11868de6b4d1972bf0547285ad9da5c7d49
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-12-13 16:56:43 +0200

    Login and Register Page Part 1 >> HTML For Login Release #5

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,8 +1,46 @@
+import "./style.css";
+import { Formik, Form } from "formik";
+import { Link } from "react-router-dom";
 export default function Login() {
   return (
     <>
-      <div>
-        <h1>This is Login Page</h1>
+      <div className="login">
+        <div className="login_wrapper">
+          <div className="login_wrap">
+            <div className="login_1">
+              <img src="../../icons/facebook.svg" alt="" />
+              <span>
+                Connect with friends and the world around you on Facebook.
+              </span>
+            </div>
+            <div className="login_2">
+              <div className="login_2_wrap">
+                <Formik>
+                  {(formik) => (
+                    <Form>
+                      <input type="text" />
+                      <input type="text" />
+                      <button type="submit" className="blue_btn">
+                        Log In
+                      </button>
+                    </Form>
+                  )}
+                </Formik>
+                <Link to="/forgot" className="forgot_password">
+                  Forget password?
+                </Link>
+                <div className="sign_splitter"></div>
+                <button className="blue_btn open_signup">
+                  Create new account
+                </button>
+              </div>
+              <Link to="/" className="sign_extra">
+                <b>Create a Page</b>for a celebrity, brand or business.
+              </Link>
+            </div>
+          </div>
+          <div className="register"></div>
+        </div>
       </div>
     </>
   );
