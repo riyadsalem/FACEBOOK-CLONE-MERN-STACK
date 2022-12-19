428288245582bfcda68f5880ea65e32ff375b5b9
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-12-19 15:11:59 +0200

    Header base Part 1 Release #9

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,9 +1,8 @@
+import Header from "../../components/header";
 export default function Home() {
   return (
-    <>
-      <div>
-        <h1>This is Home Page</h1>
-      </div>
-    </>
+    <div>
+      <Header />
+    </div>
   );
 }
