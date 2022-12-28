eab1d481802d9adec4d8d67fa0dc7747e8e42931
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-12-28 12:11:36 +0200

    Emoji picker Release #16

--- frontend/src/components/createPostPopup/index.js
+++ frontend/src/components/createPostPopup/index.js
@@ -1,8 +1,27 @@
-import { useState } from "react";
 import "./style.css";
+import { useRef, useState, useEffect } from "react";
+import Picker from "emoji-picker-react";
+
 export default function CreatePostPopup({ user }) {
   const [text, setText] = useState("");
   const [showPrev, setShowPrev] = useState(false);
+  const [picker, setPicker] = useState(false);
+  const [cursorPosition, setCursorPosition] = useState();
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    textRef.current.selectionEnd = cursorPosition;
+  }, [cursorPosition]);
+
+  const handleEmoji = (e, { emoji }) => {
+    const ref = textRef.current;
+    ref.focus();
+    const start = text.substring(0, ref.selectionStart);
+    const end = text.substring(ref.selectionStart);
+    const newText = start + emoji + end;
+    setText(newText);
+    setCursorPosition(start.length + emoji.length);
+  };
   return (
     <div className="blur">
       <div className="postBox">
@@ -28,6 +47,7 @@ export default function CreatePostPopup({ user }) {
         {!showPrev && (
           <div className="flex_center">
             <textarea
+              ref={textRef}
               maxLength="100"
               value={text}
               placeholder={`What's on your mind, ${user.first_name}`}
@@ -37,7 +57,16 @@ export default function CreatePostPopup({ user }) {
           </div>
         )}
         <div className="post_emojis_wrap">
-          <div className="comment_emoji_picker rlmove"></div>
+          {picker && (
+            <div className="comment_emoji_picker rlmove">
+              <Picker onEmojiClick={handleEmoji} />
+            </div>
+          )}
+          <img src="../../../icons/colorful.png" alt="" />
+          <i
+            className="emoji_icon_large"
+            onClick={() => setPicker((prev) => !prev)}
+          ></i>
         </div>
       </div>
     </div>
