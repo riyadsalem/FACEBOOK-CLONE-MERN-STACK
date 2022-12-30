ea36ded9d45fd22c4019d5c32ded142ce0cbf17e
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-12-30 20:09:42 +0200

    Finish post submit fixing errors, validation Release #16

--- frontend/src/components/createPostPopup/imagePreview.js
+++ frontend/src/components/createPostPopup/imagePreview.js
@@ -13,11 +13,28 @@ export default function ImagePreview({
   const handleImages = (e) => {
     let files = Array.from(e.target.files);
     files.forEach((img) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(img);
-      reader.onload = (readerEvent) => {
-        setImages((images) => [...images, readerEvent.target.result]);
-      };
+      if (
+        img.type !== "image/jpeg" &&
+        img.type !== "image/png" &&
+        img.type !== "image/webp" &&
+        img.type !== "image/gif"
+      ) {
+        setError(
+          `${img.name} format is unsupported ! only Jpeg, Png, Webp, Gif are allowed.`
+        );
+        files = files.filter((item) => item.name !== img.name);
+        return;
+      } else if (img.size > 1024 * 1024 * 5) {
+        setError(`${img.name} size is too large max 5mb allowed.`);
+        files = files.filter((item) => item.name !== img.name);
+        return;
+      } else {
+        const reader = new FileReader();
+        reader.readAsDataURL(img);
+        reader.onload = (readerEvent) => {
+          setImages((images) => [...images, readerEvent.target.result]);
+        };
+      }
     });
   };
   return (
