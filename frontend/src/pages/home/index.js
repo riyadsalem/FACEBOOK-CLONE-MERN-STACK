c8dbe74aefe41fa2e21723de99b5ca281a6a8630
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-12-31 19:23:08 +0200

    Responsive post and fix home problem Release #18

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -7,15 +7,22 @@ import Stores from "../../components/home/stories";
 import CreatePost from "../../components/createPost";
 import SendVerificatio from "../../components/home/sendVerification";
 import Post from "../../components/post";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home({ setVisible, posts }) {
   const store = useSelector((store) => store);
   const { user } = store.rootReducer;
+  const middle = useRef(null);
+  const [height, setHeight] = useState();
+  useEffect(() => {
+    setHeight(middle.current.clientHeight);
+  }, [height]);
+
   return (
-    <div className="home">
+    <div className="home" style={{ height: `${height + 150}px` }}>
       <Header />
       <LeftHome user={user} />
-      <div className="home_middle">
+      <div className="home_middle" ref={middle}>
         <Stores />
         {!user.verified && <SendVerificatio user={user} />}
         <CreatePost user={user} setVisible={setVisible} />
