36709e932ed25a3987391b6a585ebe3f62e2a451
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-02-12 06:02:37 -0800

    Update cover picture part 1 Release #20

--- frontend/src/pages/profile/Cover.js
+++ frontend/src/pages/profile/Cover.js
@@ -1,12 +1,101 @@
-import { useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import useClickOutisde from "../../helpers/clickOutside";
+import Cropper from "react-easy-crop";
+import getCroppedImg from "../../helpers/getCroppedImg";
 
 export default function Cover({ cover, visitor }) {
   const [showCoverMneu, setShowCoverMenu] = useState(false);
+  const [coverPicture, setCoverPicture] = useState("");
   const menuRef = useRef(null);
+  const refInput = useRef(null);
   useClickOutisde(menuRef, () => setShowCoverMenu(false));
+  const [error, setError] = useState("");
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [zoom, setZoom] = useState(1);
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
+
+  const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+    setCroppedAreaPixels(croppedAreaPixels);
+  }, []);
+
+  const getCroppedImage = useCallback(
+    async (show) => {
+      try {
+        const img = await getCroppedImg(coverPicture, croppedAreaPixels);
+        if (show) {
+          setZoom(1);
+          setCrop({ x: 0, y: 0 });
+          setCoverPicture(img);
+        } else {
+          return img;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [croppedAreaPixels]
+  );
+
+  const coverRef = useRef(null);
+  const [width, setWidth] = useState();
+  useEffect(() => {
+    setWidth(coverRef.current.clientWidth);
+  }, [window.innerWidth]);
+
+  const handleImage = (e) => {
+    let file = e.target.files[0];
+    if (
+      file.type !== "image/jpeg" &&
+      file.type !== "image/png" &&
+      file.type !== "image/webp" &&
+      file.type !== "image/gif"
+    ) {
+      setError(`${file.name} format is not supported.`);
+      return;
+    } else if (file.size > 1024 * 1024 * 1) {
+      setError(`${file.name} is too large max 5mb allowed.`);
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = (event) => {
+      setCoverPicture(event.target.result);
+    };
+  };
+
   return (
-    <div className="profile_cover" ref={menuRef}>
+    <div className="profile_cover" ref={coverRef}>
+      <input
+        type="file"
+        ref={refInput}
+        hidden
+        accept="image/jpeg,image/png,image/webp,image/gif"
+        onChange={handleImage}
+      />
+      {error && (
+        <div className="postError comment_error">
+          <div className="postError_error">{error}</div>
+          <button className="blue_btn" onClick={() => setError("")}>
+            Try again
+          </button>
+        </div>
+      )}
+      {coverPicture && (
+        <div className="cover_crooper">
+          <Cropper
+            image={coverPicture}
+            crop={crop}
+            zoom={zoom}
+            aspect={width / 350}
+            onCropChange={setCrop}
+            onCropComplete={onCropComplete}
+            onZoomChange={setZoom}
+            showGrid={true}
+            objectFit="horizontal-cover"
+          />
+        </div>
+      )}
       {cover && <img src={cover} alt="" />}
       {!visitor && (
         <div className="udpate_cover_wrapper">
@@ -18,12 +107,15 @@ export default function Cover({ cover, visitor }) {
             Add Cover Photo
           </div>
           {showCoverMneu && (
-            <div className="open_cover_menu">
+            <div className="open_cover_menu" ref={menuRef}>
               <div className="open_cover_menu_item hover1">
                 <i className="photo_icon"></i>
                 Select Photo
               </div>
-              <div className="open_cover_menu_item hover1">
+              <div
+                className="open_cover_menu_item hover1"
+                onClick={() => refInput.current.click()}
+              >
                 <i className="upload_icon"></i>
                 Upload Photo
               </div>
