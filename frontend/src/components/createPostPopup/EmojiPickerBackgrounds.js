9fa139f9bcd0e9b786aa72dec8a28017447403bc
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-02-15 07:27:09 -0800

    Make all popups responsive Release #20

--- frontend/src/components/createPostPopup/EmojiPickerBackgrounds.js
+++ frontend/src/components/createPostPopup/EmojiPickerBackgrounds.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import Picker from "emoji-picker-react";
+import { useMediaQuery } from "react-responsive";
 
 export default function EmojiPickerBackgrounds({
   text,
@@ -45,6 +46,10 @@ export default function EmojiPickerBackgrounds({
     bgRef.current.classList.remove("bgHandler");
   };
 
+  const sm = useMediaQuery({
+    query: "(max-width:550px)",
+  });
+
   return (
     <div className={type2 ? "images_input" : ""}>
       <div className={!type2 ? "flex_center" : ""} ref={bgRef}>
@@ -53,7 +58,8 @@ export default function EmojiPickerBackgrounds({
           maxLength="250"
           value={text}
           placeholder={`What's on your mind, ${user.first_name}`}
-          className={`post_input ${type2 ? "input2" : ""}`}
+          className={`post_input ${type2 ? "input2" : ""} 
+          ${sm && !background && "l0"}`}
           onChange={(e) => setText(e.target.value)}
           style={{
             paddingTop: `${
